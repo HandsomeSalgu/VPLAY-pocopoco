804acb06e8a30680f3c638428462855a49c42818
Salgu <warsfac@gmail.com>
2025-09-04 13:28:58 +0900

    250904 배포용

--- src/main/java/pocopoco_vplay/VplayApplication.java
+++ src/main/java/pocopoco_vplay/VplayApplication.java
@@ -3,12 +3,18 @@ package pocopoco_vplay;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
-public class VplayApplication {
+public class VplayApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(VplayApplication.class, args);
+	}
+	
+	@Override protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) { 
+		return builder.sources(VplayApplication.class); 
 	}
 
 }
